1361b81f2ae88de57b62e70675537948b4cb04cf
stefa <stefaniiaosipova@gmail.com>
2021-11-04 17:17:37 +0200

    сделался предикт, все работает, но все выглядит очень плохо, это сохранение создано чтобы если я что-то сломаю при приведении к нормальному виду, можно было начать заново.

--- main.cpp
+++ main.cpp
@@ -12,16 +12,21 @@ struct Flat{
     double cost;
 };
 
-void fill (vector <Flat> &flats_to_fill){
+
+
+void fill (bool is_it_first_run, vector <Flat> &flats_to_fill) {
     while (true) {
         Flat tmpFlat;
-
-        cout << "Enter the square value (enter 0 to stop):" << endl;
+        if (is_it_first_run){
+            cout << "Enter the square value (enter 0 to stop):" << endl;
+        }else{
+            cout << "Enter the square value for your flat:" << endl;
+        }
         cin >> tmpFlat.square;
         if (tmpFlat.square == 0) break;
         if (tmpFlat.square > 500 || tmpFlat.square <= 10) {
             cout << "incorrect value, please try again" << endl;
-            fill(flats_to_fill);                              //при вводі якогось дивного значення ми відправляємо
+            fill(true,flats_to_fill);                //при вводі якогось дивного значення ми відправляємо
             break;                                               //юзера перезаповнити параметри квартири, а цю квартиру
         }                                                       //із дивними параметрами до вектору додано не буде
 
@@ -29,7 +34,7 @@ void fill (vector <Flat> &flats_to_fill){
         cin >> tmpFlat.num_bedr;
         if (tmpFlat.num_bedr > 5 || tmpFlat.num_bedr < 1) {
             cout << "incorrect value, please try again" << endl;
-            fill(flats_to_fill);
+            fill(true,flats_to_fill);
             break;
         }
 
@@ -37,7 +42,7 @@ void fill (vector <Flat> &flats_to_fill){
         cin >> tmpFlat.num_wc;
         if (tmpFlat.num_wc > 5 || tmpFlat.num_wc < 1) {
             cout << "incorrect value, please try again" << endl;
-            fill(flats_to_fill);
+            fill(true,flats_to_fill);
             break;
         }
 
@@ -45,21 +50,24 @@ void fill (vector <Flat> &flats_to_fill){
         cin >> tmpFlat.dist_min;
         if (tmpFlat.dist_min > 180 || tmpFlat.dist_min < 1) {
             cout << "incorrect value, please try again" << endl;
-            fill(flats_to_fill);
+            fill(true,flats_to_fill);
             break;
         }
 
         cout << "Enter the type of flat - 1 for commercial or 0 for living:" << endl;
         cin >> tmpFlat.is_commercial;
 
-        cout << "Enter the cost:" << endl;
-        cin >> tmpFlat.cost;
-        if (tmpFlat.cost > 1000000000 || tmpFlat.cost < 100000) {
-            cout << "incorrect value, please try again" << endl;
-            fill(flats_to_fill);
-            break;
+        if (is_it_first_run) {
+            cout << "Enter the cost:" << endl;
+            cin >> tmpFlat.cost;
+            if (tmpFlat.cost > 1000000000 || tmpFlat.cost < 100000) {
+                cout << "incorrect value, please try again" << endl;
+                fill(true,flats_to_fill);
+                break;
+            }
         }
         flats_to_fill.push_back(tmpFlat);
+        if (is_it_first_run != true) break;
     }
 }
 
@@ -91,24 +99,41 @@ double learn (vector <Flat> flats_db){
 //    cout << points[1] << endl;
 //    cout << factor.at(0) << endl;
 //    cout << factor.at(1) << endl;
-//    cout << factor_average << endl;
+    cout << factor_average << endl;
     return factor_average;
 }
 
-void predict (double &average_factor){
 
+void predict (double &average_factor, vector <Flat> &tmpFlat){
+    fill (false, tmpFlat);
+    int points = 0;
+
+        points = tmpFlat.at(0).square * 1 + tmpFlat.at(0).num_bedr * 20 + tmpFlat.at(0).num_wc * 10;
+        if (tmpFlat.at(0).is_commercial) points += 100;
+        if (tmpFlat.at(0).dist_min < 5) {
+            points += 50;
+        } else if (tmpFlat.at(0).dist_min < 10){
+            points += 20;
+        } else if (tmpFlat.at(0).dist_min < 20) {
+            points += 10;
+        }
+
+    cout << "average factor is  " << average_factor << endl;
+    cout << "points for this flat " << points << endl;
+    cout << "average cost for the flat like this is " << points*average_factor << endl;
 }
+
+
 
 int main() {
 
-
     vector <Flat> flats;
+    vector <Flat> flatToPredict;
 
-    fill(flats);
-
+    fill(true,flats);
     double average_factor = learn(flats);
 
-    predict(average_factor);
+    predict(average_factor, flatToPredict);
 
     return 0;
 }
