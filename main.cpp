838f960e662a7b6555e850da89e017d5317c4e17
stefa <stefaniiaosipova@gmail.com>
2021-11-08 14:49:43 +0200

    вроде бы все чистенько, немножко комментариев, вроде все готово...

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//структура або параметри квартири, тип даних Flat
 struct Flat{
     double square;
     int num_bedr;
@@ -13,23 +14,24 @@ struct Flat{
 };
 
 
-
-void fill (bool is_it_first_run, vector <Flat> &flats_to_fill) {
-    while (true) {
-        Flat tmpFlat;
-        if (is_it_first_run){
+//функція заповнення початкових даних та пошукових параметрів,
+//навчальні знаення записуються у вектор даних flats типу Flat
+void fill (bool is_it_first_run, vector <Flat> &flats_to_fill) {   //булієва функція is_it_first_run позначає перший
+    while (true) {                                                 //навчальний запуск функції при значенні true
+        Flat tmpFlat;                                              //та другий, "предиктивний", при значенні false,
+        if (is_it_first_run){                                      //відрізняються вони наявністю запису ціни квартири
             cout << "Enter the square value (enter 0 to stop):" << endl;
         }else{
             cout << "Enter the square value for your flat:" << endl;
         }
         cin >> tmpFlat.square;
         if (tmpFlat.square == 0) break;
-        if (tmpFlat.square > 500 || tmpFlat.square <= 10) {
-            cout << "incorrect value, please try again" << endl;
-            fill(true,flats_to_fill);                //при вводі якогось дивного значення ми відправляємо
-            break;                                               //юзера перезаповнити параметри квартири, а цю квартиру
-        }                                                       //із дивними параметрами до вектору додано не буде
-
+        if (tmpFlat.square > 500 || tmpFlat.square < 10) {         //sanity check: при вводі якогось дивного значення
+            cout << "incorrect value, please try again" << endl;   //ми відправляємо юзера перезаповнити параметри
+            fill(true,flats_to_fill);                //квартири за допомогою рекурсії, а цю квартиру
+            break;                                                 //із дивними параметрами до вектору додано не буде.
+        }                                                          //перевірка типу даних не є суттєвою, тому що
+                                                                   //у випадку неправильного вводу випаде помилка
         cout << "Enter the number of bedrooms:" << endl;
         cin >> tmpFlat.num_bedr;
         if (tmpFlat.num_bedr > 5 || tmpFlat.num_bedr < 1) {
@@ -60,61 +62,56 @@ void fill (bool is_it_first_run, vector <Flat> &flats_to_fill) {
         if (is_it_first_run) {
             cout << "Enter the cost:" << endl;
             cin >> tmpFlat.cost;
-            if (tmpFlat.cost > 1000000000 || tmpFlat.cost < 100000) {
+            if (tmpFlat.cost > 1000000000 || tmpFlat.cost < 50000) {
                 cout << "incorrect value, please try again" << endl;
                 fill(true,flats_to_fill);
                 break;
             }
         }
         flats_to_fill.push_back(tmpFlat);
-        if (is_it_first_run != true) break;
+        if (!is_it_first_run) break;  //закінчення функції після вводу "предиктивної" частини
     }
 }
 
+//функція навчання та розрахунку балів квартир з початковіх даних
 double learn (vector <Flat> flats_db){
     vector <int> points;
     for (int i = 0; i < flats_db.size(); ++i) {
         int tmp_points = flats_db.at(i).square*1 + flats_db.at(i).num_bedr*20 + flats_db.at(i).num_wc*10;
         if (flats_db.at(i).is_commercial) tmp_points += 100;
-        if (flats_db.at(i).dist_min < 5) {
-            tmp_points += 50;
-        } else if (flats_db.at(i).dist_min < 10){
-            tmp_points += 20;
-        } else if (flats_db.at(i).dist_min < 20) {
+        if (flats_db.at(i).dist_min < 6) {                     //у системі оцінювання квартири за кожен м2 видається
+            tmp_points += 50;                                  //1 поінт, за кожну кімнату/спальню 20, за санвузол 10,
+        } else if (flats_db.at(i).dist_min < 11) {             //якщо приміщення комерційне, додається 100 поінтів,
+            tmp_points += 20;                                  //за простою системою додаються бали за відстань до метро
+        } else if (flats_db.at(i).dist_min < 21) {
             tmp_points += 10;
         }
         points.push_back(tmp_points);
     }
 
-    vector <double> factor;
+    //невеличкий метод розрахування середньої вартості одного поінту, множника
     double factor_average = 0;
     double tmp_sum = 0;
     for (int i = 0; i < flats_db.size(); ++i) {
         double tmp_factor = flats_db.at(i).cost / points[i];
-        factor.push_back(tmp_factor);
         tmp_sum += tmp_factor;
         factor_average = tmp_sum / flats_db.size();
     }
-//    cout << points[0] << endl;
-//    cout << points[1] << endl;
-//    cout << factor.at(0) << endl;
-//    cout << factor.at(1) << endl;
-    cout << factor_average << endl;
     return factor_average;
 }
 
-
+//функція саме розрахунку вартості шуканої квартири шляхом підрахування її балів та домноження їх на ср. вартість поінту
 void predict (double &average_factor, vector <Flat> &tmpFlat){
-    fill (false, tmpFlat);
-    int points = 0;
+    fill (false, tmpFlat);  //те саме друге заповнення параметрів квартири без вводу ціни
 
+    int points = 0;
         points = tmpFlat.at(0).square * 1 + tmpFlat.at(0).num_bedr * 20 + tmpFlat.at(0).num_wc * 10;
         if (tmpFlat.at(0).is_commercial) points += 100;
-        if (tmpFlat.at(0).dist_min < 5) {
+        if (tmpFlat.at(0).dist_min < 6) {
             points += 50;
-        } else if (tmpFlat.at(0).dist_min < 10){
+        } else if (tmpFlat.at(0).dist_min < 11){
             points += 20;
-        } else if (tmpFlat.at(0).dist_min < 20) {
+        } else if (tmpFlat.at(0).dist_min < 21) {
             points += 10;
         }
 
